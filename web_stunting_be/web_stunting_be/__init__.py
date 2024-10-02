ad5b3e93ea399b0008a36daf65b9059d88d43a87
daffa <daffamiko123@gmail.com>
2024-10-02 15:54:00 +0700

    ignore env

--- web_stunting_be/web_stunting_be/__init__.py
+++ web_stunting_be/web_stunting_be/__init__.py
@@ -1,7 +1,4 @@
 from pyramid.config import Configurator
-# from pyramid.authorization import ACLAuthorizationPolicy
-# from pyramid.authentication import AuthTktAuthenticationPolicy
-
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
